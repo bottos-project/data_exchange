8bfa1f1a109d6a8553fafc64e278eadf15f4aa39
YuanJia <cnyuanjia@gmail.com>
2018-01-18 11:45:00 +0800

    个人设置页面+历史交易修改

--- app/containers/Profile/Account/index.js
+++ app/containers/Profile/Account/index.js
@@ -1,11 +1,18 @@
 import React,{PureComponent} from 'react'
-import { Tabs, Input, Ico,Button } from 'antd';
+import { Tabs, Input, Ico,Button,Select } from 'antd';
 const TabPane = Tabs.TabPane;
 import './styles.less';
 
 function callback(key) {
     console.log(key)
 }
+
+const Option = Select.Option;
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
+}
+
 export default class BTAccount extends PureComponent{
     constructor(props){
         super(props)
@@ -13,27 +20,89 @@ export default class BTAccount extends PureComponent{
 
     render(){
         return(
-            <div>BTAccount
+            <div>
                 <Tabs defaultActiveKey="1" onChange={callback}>
                     <TabPane tab="个人资料" key="1">
                         <div className="personalInformation">
                             <div className="UserName">
                                 <span>UserName:</span>
-                                <span>Jack</span>
-                                <Input style={{width:"80px"}} placeholder="changing username" />
-                                <Button>修改</Button>
+                                <Input style={{width:"80px",height:"20px"}} value="Jack" />
+                                <Button>modify</Button>
                             </div>
                             <div className="realName">
                                 <span>realName:</span>
                                 <span>Tom</span>
-                                <Input style={{width:"80px"}} placeholder="changing username" />
+                            </div>
+                            <div className="accountType">
+                                <span>accountType:</span>
+                                    <Select defaultValue="provider" style={{ width:"120px", marginTop:"-30px"}} onChange={handleChange}>
+                                        <Option value="demandSide">Demand side</Option>
+                                        <Option value="provider">Provider</Option>
+                                        <Option value="both">both</Option>
+                                    </Select>
+                                <Button>modify</Button>
+                            </div>
+                            <div className="phoneNumber">
+                                <span>phoneNumber:</span>
+                                <Input style={{width:"120px",height:"20px"}} value="13761234905" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="mailBox">
+                                <span>mailBox:</span>
+                                <Input style={{width:"160px",height:"20px"}} value="cnyuanjia@gmail.com" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="address">
+                                <span>address:</span>
+                                <Input style={{width:"160px",height:"20px"}} value="" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="submit" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                <Button>submit</Button>
                             </div>
                         </div>
-                    </TabPane>
-
-                    <TabPane tab="安全设置" key="2">
-
-
+                        <div className="enterpriseInformation">
+                            <div className="UserName">
+                                <span>UserName:</span>
+                                <Input style={{width:"80px",height:"20px"}} value="Jack" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="enterpriseName">
+                                <span>enterpriseName:</span>
+                                <span>enterpriseName</span>
+                            </div>
+                            <div className="accountType">
+                                <span>accountType:</span>
+                                <Select defaultValue="provider" style={{ width:"120px", marginTop:"-30px"}} onChange={handleChange}>
+                                    <Option value="demandSide">Demand side</Option>
+                                    <Option value="provider">Provider</Option>
+                                    <Option value="both">both</Option>
+                                </Select>
+                                <Button>modify</Button>
+                            </div>
+                            <div className="phoneNumber">
+                                <span>enterprisePhoneNumber:</span>
+                                <Input style={{width:"120px",height:"20px"}} value="888-888-888" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="mailBox">
+                                <span>enterpriseMailBox:</span>
+                                <Input style={{width:"160px",height:"20px"}} value="cn@gmail.com" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="address">
+                                <span>enterpriseAddress:</span>
+                                <Input style={{width:"160px",height:"20px"}} value="" />
+                                <Button>modify</Button>
+                            </div>
+                            <div className="code">
+                                <span>组织机构代码:</span>
+                                <span>000</span>
+                            </div>
+                            <div className="submit" style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+                                <Button>submit</Button>
+                            </div>
+                        </div>
                     </TabPane>
                 </Tabs>
             </div>
