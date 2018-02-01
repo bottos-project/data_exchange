daecfd78dc1849538c99290e3a87914ad59d1a22
yuanjunliang <1610712512@qq.com>
2018-02-01 17:37:38 +0800

    config iconfont config and optimize fetch request to BTFetch

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -34,7 +34,7 @@ module.exports = {
 
   plugins: [
     // webpack 内置的 banner-plugin
-    new webpack.BannerPlugin("Copyright by wangfupeng1988@github.com."),
+    new webpack.BannerPlugin("Copyright by bottos."),
 
     // html 模板插件
     new HtmlWebpackPlugin({
