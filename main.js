41d7d8254d277740caab448c80d62972d8f38a02
VinciXie <szqq602@gmail.com>
2018-06-05 01:32:47 +0800

    add copyright

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-// const {app, BrowserWindow} = require('electron')
-const electron = require('electron');
-const {app, BrowserWindow} = electron;
+/*
+  Copyright 2017~2022 The Bottos Authors
+  This file is part of the Bottos Data Exchange Client
+  Created by Developers Team of Bottos.
+
+  This program is free software: you can distribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Bottos. If not, see <http://www.gnu.org/licenses/>.
+*/
+const {app, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
 const pkg = require('./package.json')
