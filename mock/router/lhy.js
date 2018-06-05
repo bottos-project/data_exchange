f490e929e586f45f0b85ec9fbc2f35b0cb8bd845
yuanjunliang <1610712512@qq.com>
2018-06-05 11:47:44 +0800

    add Number Input component

--- mock/router/lhy.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-const lhy = require('../data/lhy')
-
-
-router.post('/user/register',lhy.user_register)
-
-
-module.exports = router.routes()
--- mock/router/yj.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-const yj = require('../data/yj')
-
-
-
-
-
-module.exports = router.routes()
